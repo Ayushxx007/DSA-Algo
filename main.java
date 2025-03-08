09e877d0affae4a852ba71164be38a459c862219
“Ayush <ayushkumawat77889@gmail.com>
2025-03-08 08:24:55 +0530

    Trie

--- main.java
+++ main.java
@@ -1,8 +1,83 @@
-public class main{
-    public static void main(String[]args){
 
-        System.out.println("xyz");
+public class main {
+    public static void main(String[] args) {
+        TrieNode root =new TrieNode();
+        insert("mahika",root);
+        insert("ganduka",root);
+        insert("panduka",root);
+        insert("manduka",root);
+        System.out.println( insert("yamuna",root));
+        System.out.println(search("gandukaa",root));
+        System.out.println(startsWith("gz",root));
+
+
+
+
+
 
     }
-    
+    public static boolean insert(String word, TrieNode root){
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            int index=word.charAt(i)-'a';
+            if(curr.children[index]==null){
+                curr.children[index]=new TrieNode();
+
+
+            }
+            curr=curr.children[index];
+
+        }
+        return curr.isEnd=true;
+
+
+
+
+
+    }
+    public static boolean search(String word,TrieNode root){
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            int index = word.charAt(i)-'a';
+            if(curr.children[index]==null){
+                return false;
+
+            }
+            curr=curr.children[index];
+
+        }
+        return curr.isEnd;
+
+
+
+    }
+    public static boolean startsWith(String word,TrieNode root){
+        TrieNode curr=root;
+        for(int i=0;i<word.length();i++){
+            int index=word.charAt(i)-'a';
+            if(curr.children[index]==null){
+                return false;
+            }
+            curr=curr.children[index];
+        }
+        return true;
+
+
+    }
+
+}
+
+
+class TrieNode{
+    TrieNode children[];
+    boolean isEnd;
+    TrieNode(){
+        children=new TrieNode[26];
+        isEnd=false;
+
+    }
+
+
+
+
 }
